3d5805005c7ead7e8e593952b349677c5f2f7190
xysong00 <xuyansong2011@163.com>
2013-05-28 10:43:34 +0800

    Update AddressBook.java

--- AddressBook.java
+++ AddressBook.java
@@ -9,105 +9,104 @@ import java.util.Set;
  * Functions display data, add person and remove person are also included.
  */
 public class AddressBook{
-  	/**
-	 * The HashTable used to restore data of persons.
-	 */
-	private Hashtable<String,Hashtable<String,Person>> data = 
-			new Hashtable<String,Hashtable<String,Person>>();
+    
+    /**
+     * The HashTable used to restore data of persons.
+     */
+     private Hashtable<String,Hashtable<String,Person>> data = new Hashtable<String,Hashtable<String,Person>>();
 	
-	/**
-	 * The string representing the current position in data.
-	 */
-	private String currentDirectory = null;
+    /**
+     * The string representing the current position in data.
+     */
+     private String currentDirectory = null;
+
+    /**
+     * Using singleton mode to get a instance of AddressBook.
+     */
+     private static AddressBook instance = null;
+
+    /**
+     * <b>Description:</b><br>
+     * The default constructor of class AddressBook.
+     */
+     private AddressBook(){
+         Person lilei = new Person("lilei", 27, "13700000000", "Earth somewhere");
+	 Person hanmeimei = new Person("hanmeimei", 26, "13700000001", "Earth somewhere else");
+ 	 Hashtable<String,Person> entries = new Hashtable<String, Person>();
+	 entries.put("lilei", lilei);
+	 entries.put("hanmeimei", hanmeimei);
+	 this.data.put("entries", entries);
+	 this.currentDirectory = "root";
+     }
 	
-	/**
-	 * Using singleton mode to get a instance of AddressBook.
-	 */
-	private static AddressBook instance = null;
+    /**
+     * <b>Description:</b><br>
+     * Get the single instance of AddressBook class.
+     * 
+     * @return AddressBook - the single instance 
+     */
+     public static AddressBook getInstance(){
+	 if(instance == null){
+	     synchronized (AddressBook.class){
+	         if(instance == null){
+		     instance = new AddressBook();
+                 }
+	     }
+	 }
+	 return instance;
+     }
 	
-	/**
-	 * <b>Description:</b><br>
-	 * The default constructor of class AddressBook.
-	 */
-	private AddressBook(){
-		Person lilei = new Person("lilei", 27, "13700000000", "Earth somewhere");
-		Person hanmeimei = new Person("hanmeimei", 26, "13700000001", "Earth somewhere else");
-		Hashtable<String,Person> entries = new Hashtable<String, Person>();
-		entries.put("lilei", lilei);
-		entries.put("hanmeimei", hanmeimei);
-		this.data.put("entries", entries);
-		this.currentDirectory = "root";
-	}
+    /**
+     * <b>Description:</b><br>
+     * Get the current position in data.
+     * 
+     * @return String - the current position - Example: "entries" 
+     */
+     public String getCurrentDirectory() {
+	 return this.currentDirectory;
+     }
 	
-	/**
-	 * <b>Description:</b><br>
-	 * Get the single instance of AddressBook class.
-	 * 
-	 * @return AddressBook - the single instance 
-	 */
-	public static AddressBook getInstance(){
-		if(instance == null){
-			synchronized (AddressBook.class){
-				if(instance == null){
-					instance = new AddressBook();
-				}
-			}
-		}
-		return instance;
-	}
+    /**
+     * <b>Description:</b><br>
+     * Change the current position to objective position.
+     * 
+     * @param directory - String - objective position - Example: "entries" 
+     */
+     public void changeDirectory(String directory) {
+	 this.currentDirectory = directory;
+     }
 	
-	/**
-	 * <b>Description:</b><br>
-	 * Get the current position in data.
-	 * 
-	 * @return String - the current position - Example: "entries" 
-	 */
-	public String getCurrentDirectory() {
-		return this.currentDirectory;
-	}
+    /**
+     * <b>Description:</b><br>
+     * Change the current position to root.
+     */
+     public void changeDirectoryToRoot() {
+         this.currentDirectory = "root";
+     }
 	
-	/**
-	 * <b>Description:</b><br>
-	 * Change the current position to objective position.
-	 * 
-	 * @param directory - String - objective position - Example: "entries" 
-	 */
-	public void changeDirectory(String directory) {
-		this.currentDirectory = directory;
-	}
-	
-	/**
-	 * <b>Description:</b><br>
-	 * Change the current position to root.
-	 */
-	public void changeDirectoryToRoot() {
-		this.currentDirectory = "root";
-	}
-	
-	/**
-	 * <b>Description:</b><br>
-	 * Get all items in current directory
-	 * 
-	 * return Set<String> - set of all items' names
-	 */
-	public Set<String> getCurrentItems(){
-		if(this.currentDirectory == "root") {
-			return this.data.keySet();
-		} else {
-			return this.data.get(this.currentDirectory).keySet();
-		}
-	}
-	
-	/**
-	 * <b>Description:</b><br>
-	 * Get a specified person's detail information in String form.
-	 * 
-	 * @param name - String - the name of a specified person - Example: "lilei"
-	 * 
-	 * @return String 
-	 *                   
-	 */
-	public String getPersonData(String name) {
+    /**
+     * <b>Description:</b><br>
+     * Get all items in current directory
+     * 
+     * return Set<String> - set of all items' names
+     */
+     public Set<String> getCurrentItems(){
+	 if(this.currentDirectory == "root") {
+	     return this.data.keySet();
+	 } else {
+	     return this.data.get(this.currentDirectory).keySet();
+	 }
+     }
+
+    /**
+     * <b>Description:</b><br>
+     * Get a specified person's detail information in String form.
+     * 
+     * @param name - String - the name of a specified person - Example: "lilei"
+     * 
+     * @return String 
+     * /
+     public String getPersonData(String name) {
 		return this.data.get("entries").get(name).getInformation();
 	}
 	
