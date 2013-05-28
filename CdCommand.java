446cb24a71d88f6f088c9a3e7102e182e153043c
xysong00 <xuyansong2011@163.com>
2013-05-28 11:37:45 +0800

    Update CdCommand.java

--- CdCommand.java
+++ CdCommand.java
@@ -10,46 +10,46 @@ import ericsson.online.exam.AddressBook;
  * </p>
  */
 public class CdCommand {
-  private String directory;
-	private String output = "";
-	public CdCommand(String directory) {
-		this.directory = directory;
-	}
+    private String directory;
+    private String output = "";
+    public CdCommand(String directory) {
+        this.directory = directory;
+    }
 	
-	/**
-	 * <b>Description:</b>
-	 * Execute the cd command and fill output with result. Finally 
-	 * return the executed command.
-	 * 
-	 * @return CdCommand
-	 */
-	public CdCommand exec() {
-		AddressBook addressBook = AddressBook.getInstance();
-		if(directory == null) {
-			//cd command with no parameter, return to root
-			addressBook.changeDirectoryToRoot();
-			this.output = "Done";
-		} else if(addressBook.existInCurrentDirectory(directory)) {
-			//cd command with parameter, change to directory
-			addressBook.changeDirectory(directory);
-			this.output = "Done";
-		} else {
-			this.output = directory + " does not exist.";
-		}
-		return this;
+    /**
+     * <b>Description:</b>
+     * Execute the cd command and fill output with result. Finally 
+     * return the executed command.
+     * 
+     * @return CdCommand
+     */
+    public CdCommand exec() {
+	AddressBook addressBook = AddressBook.getInstance();
+	if(directory == null) {
+	    //cd command with no parameter, return to root
+	    addressBook.changeDirectoryToRoot();
+	    this.output = "Done";
+	} else if(addressBook.existInCurrentDirectory(directory)) {
+	    //cd command with parameter, change to directory
+	    addressBook.changeDirectory(directory);
+	    this.output = "Done";
+	} else {
+	    this.output = directory + " does not exist.";
 	}
+	return this;
+    }
 	
-	/**
-	 * <b>Description:</b><br>
-	 * Get the output of this command;
-	 * 
-	 * @return String - output information
-	 */
-	public String getOutput() {
-		if(this.output.equals("")) {
-			return "Cd command not executed.";
-		} else {
-			return this.output;
-		}
+    /**
+     * <b>Description:</b><br>
+     * Get the output of this command;
+     * 
+     * @return String - output information
+     */
+    public String getOutput() {
+	if(this.output.equals("")) {
+            return "Cd command not executed.";
+	} else {
+	    return this.output;
 	}
+    }
 }
