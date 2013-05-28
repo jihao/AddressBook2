f83bade87e8b4a0df1ddce1710eb6b1feb45cd82
xysong00 <xuyansong2011@163.com>
2013-05-28 10:57:38 +0800

    Update AddressBook.java

--- AddressBook.java
+++ AddressBook.java
@@ -13,23 +13,23 @@ public class AddressBook{
     /**
      * The HashTable used to restore data of persons.
      */
-     private Hashtable<String,Hashtable<String,Person>> data = new Hashtable<String,Hashtable<String,Person>>();
+    private Hashtable<String,Hashtable<String,Person>> data = new Hashtable<String,Hashtable<String,Person>>();
 	
     /**
      * The string representing the current position in data.
      */
-     private String currentDirectory = null;
+    private String currentDirectory = null;
 
     /**
      * Using singleton mode to get a instance of AddressBook.
      */
-     private static AddressBook instance = null;
+    private static AddressBook instance = null;
 
     /**
      * <b>Description:</b><br>
      * The default constructor of class AddressBook.
      */
-     private AddressBook(){
+    private AddressBook(){
          Person lilei = new Person("lilei", 27, "13700000000", "Earth somewhere");
 	 Person hanmeimei = new Person("hanmeimei", 26, "13700000001", "Earth somewhere else");
  	 Hashtable<String,Person> entries = new Hashtable<String, Person>();
@@ -45,16 +45,16 @@ public class AddressBook{
      * 
      * @return AddressBook - the single instance 
      */
-     public static AddressBook getInstance(){
-	 if(instance == null){
-	     synchronized (AddressBook.class){
-	         if(instance == null){
-		     instance = new AddressBook();
-                 }
-	     }
-	 }
-	 return instance;
-     }
+    public static AddressBook getInstance(){
+	if(instance == null){
+	    synchronized (AddressBook.class){
+	        if(instance == null){
+		    instance = new AddressBook();
+                }
+	    }
+	}
+	return instance;
+    }
 	
     /**
      * <b>Description:</b><br>
@@ -62,9 +62,9 @@ public class AddressBook{
      * 
      * @return String - the current position - Example: "entries" 
      */
-     public String getCurrentDirectory() {
-	 return this.currentDirectory;
-     }
+    public String getCurrentDirectory() {
+        return this.currentDirectory;
+    }
 	
     /**
      * <b>Description:</b><br>
@@ -72,17 +72,17 @@ public class AddressBook{
      * 
      * @param directory - String - objective position - Example: "entries" 
      */
-     public void changeDirectory(String directory) {
-	 this.currentDirectory = directory;
-     }
+    public void changeDirectory(String directory) {
+	this.currentDirectory = directory;
+    }
 	
     /**
      * <b>Description:</b><br>
      * Change the current position to root.
      */
-     public void changeDirectoryToRoot() {
-         this.currentDirectory = "root";
-     }
+    public void changeDirectoryToRoot() {
+        this.currentDirectory = "root";
+    }
 	
     /**
      * <b>Description:</b><br>
@@ -90,13 +90,13 @@ public class AddressBook{
      * 
      * return Set<String> - set of all items' names
      */
-     public Set<String> getCurrentItems(){
-	 if(this.currentDirectory == "root") {
-	     return this.data.keySet();
-	 } else {
-	     return this.data.get(this.currentDirectory).keySet();
-	 }
-     }
+    public Set<String> getCurrentItems(){
+	if(this.currentDirectory == "root") {
+	    return this.data.keySet();
+	} else {
+	    return this.data.get(this.currentDirectory).keySet();
+	}
+    }
 
     /**
      * <b>Description:</b><br>
@@ -106,9 +106,9 @@ public class AddressBook{
      * 
      * @return String - a person's information
      */
-     public String getPersonData(String name) {
-         return this.data.get("entries").get(name).getInformation();
-     }
+    public String getPersonData(String name) {
+        return this.data.get("entries").get(name).getInformation();
+    }
 	
     /**
      * <b>Description:</b><br>
@@ -119,9 +119,9 @@ public class AddressBook{
      * @param mobile - String - the mobile phone number - Example: "13800000000"
      * @param address - String - the address of a person - Example: "Shanghai"
      */
-     public void addPerson(String name, int age, String mobile, String address) {
-         this.data.get(this.currentDirectory).put(name, new Person(name, age, mobile, address));
-     }
+    public void addPerson(String name, int age, String mobile, String address) {
+        this.data.get(this.currentDirectory).put(name, new Person(name, age, mobile, address));
+    }
 	
     /**
      * <b>Description:</b><br>
@@ -131,13 +131,13 @@ public class AddressBook{
      * 
      * @return boolean 
      */
-     public boolean existInCurrentDirectory(String name) {
-	 if(currentDirectory.equals("root")) {
-	     return this.data.containsKey(name);
-	 } else {
-	     return this.data.get(currentDirectory).containsKey(name);
-	 }
-     }
+    public boolean existInCurrentDirectory(String name) {
+	if(currentDirectory.equals("root")) {
+	    return this.data.containsKey(name);
+	} else {
+	    return this.data.get(currentDirectory).containsKey(name);
+	}
+    }
 	
     /**
      * <b>Description:</b><br>
@@ -145,9 +145,9 @@ public class AddressBook{
      * 
      * @param name - String - the name of the person being removed - Example: "lilei"
      */
-     public void removePerson(String name) {
-         this.data.get(this.currentDirectory).remove(name);
-     }
+    public void removePerson(String name) {
+        this.data.get(this.currentDirectory).remove(name);
+    }
 }
 
 /**
@@ -156,10 +156,10 @@ public class AddressBook{
  */
 class Person {
      
-     private String name;
-     private int age;
-     private String mobile;
-     private String address;
+    private String name;
+    private int age;
+    private String mobile;
+    private String address;
 	
     /**
      * <b>Description:</b><br>
@@ -170,20 +170,20 @@ class Person {
      * @param mobile - String
      * @param address - String
      */
-     public Person(String name, int age, String mobile, String address) {
-         this.name = name;
-	 this.age = age;
-	 this.mobile = mobile;
-	 this.address = address; 
-     }
+    public Person(String name, int age, String mobile, String address) {
+        this.name = name;
+	this.age = age;
+	this.mobile = mobile;
+	this.address = address; 
+    }
 	
     /**
      * <b>Description:</b><br>
      * Display the informations of this person.
      */
-     public String getInformation() {
-         return "\"" + this.name + "\" : { \"age\": " + this.age 
-		+", \"mobile\": \"" + this.mobile + "\", \"address\": \"" 
-		+ this.address + "\"}";
-     }
+    public String getInformation() {
+        return "\"" + this.name + "\" : { \"age\": " + this.age 
+	       +", \"mobile\": \"" + this.mobile + "\", \"address\": \"" 
+	       + this.address + "\"}";
+    }
 }
