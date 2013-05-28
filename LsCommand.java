300d71a74ac52c30861fb1c3ed0a8e3333e4da04
xysong00 <xuyansong2011@163.com>
2013-05-28 11:47:55 +0800

    Update LsCommand.java

--- LsCommand.java
+++ LsCommand.java
@@ -9,34 +9,34 @@ import ericsson.online.exam.AddressBook;
  * The class handles the task to List all items in current position.
  */
 public class LsCommand {
-  private String output = "";
+    private String output = "";
 	
-	/**
-	 * <b>Description:</b><br>
-	 * Execute the cat command. Fill output with items' names. Finally
-	 * return the executed command.
-	 * 
-	 * @return LsCommand
-	 */
-	public LsCommand exec() {
-		Set<String> items = AddressBook.getInstance().getCurrentItems();
-		for(String item : items) {
-			this.output = item + " " + this.output;
-		}
-		return this;
+    /**
+     * <b>Description:</b><br>
+     * Execute the cat command. Fill output with items' names. Finally
+     * return the executed command.
+     * 
+     * @return LsCommand
+     */
+    public LsCommand exec() {
+	Set<String> items = AddressBook.getInstance().getCurrentItems();
+	for(String item : items) {
+	    this.output = item + " " + this.output;
 	}
+	return this;
+    }
 	
-	/**
-	 * <b>Description:</b><br>
-	 * Get the output of this command;
-	 * 
-	 * @return String - output information
-	 */
-	public String getOutput() {
-		if(this.output.equals("")) {
-			return "Ls command not executed.";
-		} else {
-			return this.output;
-		}
+    /**
+     * <b>Description:</b><br>
+     * Get the output of this command;
+     * 
+     * @return String - output information
+     */
+    public String getOutput() {
+	if(this.output.equals("")) {
+	    return "Ls command not executed.";
+	} else {
+	    return this.output;
 	}
+    }
 }
