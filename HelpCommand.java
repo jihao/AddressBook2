c7108c4b6359ea8e3d962df704994658bbbc14cf
xysong00 <xuyansong2011@163.com>
2013-05-28 11:41:26 +0800

    Update HelpCommand.java

--- HelpCommand.java
+++ HelpCommand.java
@@ -5,31 +5,32 @@ package ericsson.online.exam.commands;
  * The class handles the task to display the help information.
  */
 public class HelpCommand {
-  private String output = "";
 	
-	/**
-	 * <b>Description:</b><br>
-	 * Execute help command and fill output with help message.
-	 * Finally return the executed command.
-	 * 
-	 * @return HelpCommand
-	 */
-	public HelpCommand exec() {
-		this.output = "This is the help message.";
-		return this;
-	}
+    private String output = "";
 	
-	/**
-	 * <b>Description:</b><br>
-	 * Get the output of this command;
-	 * 
-	 * @return String - output information
-	 */
-	public String getOutput() {
-		if(this.output.equals("")) {
-			return "Help command not executed.";
-		} else {
-			return this.output;
-		}
+    /**
+     * <b>Description:</b><br>
+     * Execute help command and fill output with help message.
+     * Finally return the executed command.
+     * 
+     * @return HelpCommand
+     */
+    public HelpCommand exec() {
+	this.output = "This is the help message.";
+	return this;
+    }
+	
+    /**
+     * <b>Description:</b><br>
+     * Get the output of this command;
+     * 
+     * @return String - output information
+     */
+    public String getOutput() {
+	if(this.output.equals("")) {
+	    return "Help command not executed.";
+	} else {
+	    return this.output;
 	}
+    }
 }
