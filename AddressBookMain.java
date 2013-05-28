ef6ead1a8a1c534d706e57b8b7732754a6faaf89
xysong00 <xuyansong2011@163.com>
2013-05-28 13:27:16 +0800

    Update AddressBookMain.java

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -5,8 +5,8 @@ import java.util.concurrent.Executors;
 
 /**
  * <b>Description:</b><br>
- * This class is a demo which starts a main thread to manage commands 
- * input and execution.
+ * This class is a demo which starts a main thread to manage input 
+ * and commands execution.
  */
 public class AddressBookMain {
     public static void mian(String[] args) {
