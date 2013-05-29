d8914c51716f0fe8830ba49b80d278bb0f195022
xysong00 <xuyansong2011@163.com>
2013-05-29 15:22:24 +0800

    Update Commands.java

--- Commands.java
+++ Commands.java
@@ -5,25 +5,27 @@ package ericsson.online.exam.commands;
  * The class is the father of other commands.
  */
 public abstract class Commands {
-  /**
-	 * The output message
-	 */
-	private String output = "";
-	/**
-	 * <b>Description:</b>
-	 * Execute the cat command. 
-	 * 
-	 * @return Commands
-	 */
-	abstract public Commands exec();
+    
+    /**
+     * The output message
+     */
+    private String output = "";
+    
+    /**
+     * <b>Description:</b>
+     * Execute the cat command. 
+     * 
+     * @return Commands
+     */
+    abstract public Commands exec();
 	
-	/**
-	 * <b>Description:</b>
-	 * Get the output message. 
-	 * 
-	 * @return String
-	 */
-	public String getOutput() {
-		return this.output;
-	}
+    /**
+     * <b>Description:</b>
+     * Get the output message. 
+     * 
+     * @return String
+     */
+    public String getOutput() {
+ 	return this.output;
+    }
 }
