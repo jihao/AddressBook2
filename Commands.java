e1172102ded79856af0f97bd6f3bfb5a09d77e0f
xysong00 <xuyansong2011@163.com>
2013-05-29 15:20:47 +0800

    Create Commands.java

---
+++ Commands.java
@@ -0,0 +1,29 @@
+package ericsson.online.exam.commands;
+
+/**
+ * <b>Description:</b><br>
+ * The class is the father of other commands.
+ */
+public abstract class Commands {
+  /**
+	 * The output message
+	 */
+	private String output = "";
+	/**
+	 * <b>Description:</b>
+	 * Execute the cat command. 
+	 * 
+	 * @return Commands
+	 */
+	abstract public Commands exec();
+	
+	/**
+	 * <b>Description:</b>
+	 * Get the output message. 
+	 * 
+	 * @return String
+	 */
+	public String getOutput() {
+		return this.output;
+	}
+}
